09d9c8b21ec45f37043ee18c9b3f0c47051f03b3
caiofreitass <caiosilvaben3@gmail.com>
2025-09-23 19:29:00 -0300

    Update casa.lua

--- casa.lua
+++ casa.lua
@@ -1,4 +1,4 @@
--- CasaGiganteVaziaComRemoveEPhysics.lua
+-- CasaGiganteVermelhaDesancorada.lua
 local UserInput = game:GetService("UserInputService")
 local Players = game:GetService("Players")
 
@@ -72,12 +72,12 @@ local function removerCasas()
     end
 end
 
--- Função para desancorar todas as parts das casas
-local function liberarCasas()
+-- Função para desancorar todas as partes vermelhas das casas
+local function desancorarPartesVermelhas()
     for _, obj in pairs(workspace:GetChildren()) do
         if obj:IsA("Model") and obj.Name == "CasaGiganteVazia" then
             for _, part in pairs(obj:GetDescendants()) do
-                if part:IsA("BasePart") then
+                if part:IsA("BasePart") and part.BrickColor == BrickColor.new("Bright red") then
                     part.Anchored = false
                 end
             end
@@ -92,7 +92,7 @@ UserInput.InputBegan:Connect(function(input, gameProcessed)
         criarCasa()
     elseif input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode.Name == "P" then
         removerCasas()
-    elseif input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode.Name == "O" then
-        liberarCasas()
+    elseif input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode.Name == "K" then
+        desancorarPartesVermelhas()
     end
 end)
